34557a87ee309b97438a82cdebc41aeffc91c123
Eslamamro <islamamr2003@gmail.com>
2025-03-12 19:16:43 +0200

    structure adjustment

--- mywebsite/main/apps.py
+++ mywebsite/main/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ECommerceConfig(AppConfig):
+class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'e_commerce'
+    name = 'main'
